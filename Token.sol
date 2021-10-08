3810d5a7f60fcb0483881ef6ed9ebaeff73f7123
credefi <82961135+credefi@users.noreply.github.com>
2021-10-08 12:48:26 +0300

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -1,28 +1,19 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.7.0 <=0.8.7;
+pragma solidity >=0.7.0 <=0.8.9;
 
 import "./ERC20.sol";
 
 contract CREDIToken is ERC20 {
-    constructor() ERC20("TESTCR1", "TESTCR1") {
+    constructor()
+        ERC20(
+            "Credi",
+            "CREDI",
+            0x0000000000000000000000000000000000000000,
+            0x0000000000000000000000000000000000000000
+        )
+    {
         uint8 decimals = 18;
-        _mint(
-            150000000 * 10**decimals,
-            decimals,
-            0x0Ba4f79732a70c8428b18f8506d60fF7b581F349
-        );
+        _mint(150000000 * 10**decimals, decimals);
     }
 }
-
-// contract CREDIToken is ERC20 {
-//     constructor() ERC20("Credi", "CREDI") {
-//         uint8 decimals = 18;
-//         _mint(
-//             msg.sender,
-//             150000000 * 10**decimals,
-//             decimals,
-//             0x0Ba4f79732a70c8428b18f8506d60fF7b581F349
-//         );
-//     }
-// }
